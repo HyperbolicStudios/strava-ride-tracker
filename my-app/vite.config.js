1a2cf5723c259c56e6071f5cd87e61753af8148e
Mark <markinoe@gmail.com>
2026-03-13 22:15:03 -0700

    Ready for production

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '../', '');
   return {
+    base: '/strava-ride-tracker/',
     plugins: [react()],
     define: {
       'import.meta.env.VITE_STRAVA_MAPBOX_TOKEN': JSON.stringify(env.VITE_STRAVA_MAPBOX_TOKEN)
+    },
+    build: {
+      outDir: '../docs'
     }
   };
 });
