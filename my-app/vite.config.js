67b47bf7d04d8ec5eef34375efbc008a6e163bc2
Mark <markinoe@gmail.com>
2026-03-13 22:26:32 -0700

    fix

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '../', '');
   return {
-    base: '/strava-ride-tracker/',
+    base: '/',
     plugins: [react()],
     define: {
       'import.meta.env.VITE_STRAVA_MAPBOX_TOKEN': JSON.stringify(env.VITE_STRAVA_MAPBOX_TOKEN)
